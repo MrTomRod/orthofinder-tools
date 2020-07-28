1262643b53ed7f163f8017407fd0d6f777a5b946
Thomas Roder <thomas.roder@bioinformatics.unibe.ch>
2020-07-28 14:07:05 +0200

    added chances made by ViriatoII (github.com/davidemms/OrthoFinder/pull/434)
    - compatible with new N0.tsv structure
    - small tweek to ignore proteins annotated AbInitio with maker, which don't have useful information, and to be resilient to gene names missing in the corresponding fastas

--- orthogroup_to_gene_name.py
+++ orthogroup_to_gene_name.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python
+
 import os
 import pandas as pd
 from Bio import SeqIO
 
+
 class OrthogroupToGeneName():
     @staticmethod
-    def run(orthogroups_tsv: str, fasta_dir: str, write: bool = False):
+    def run(orthogroups_tsv: str, fasta_dir: str, write: bool = True):
+
         assert os.path.isfile(orthogroups_tsv), F'orthogroups_tsv does not exist: "{orthogroups_tsv}"'
         assert os.path.isdir(fasta_dir), F'fasta_dir does not exist: "{fasta_dir}"'
         if write:
-            out = os.path.dirname(orthogroups_tsv)
+            out = os.path.dirname("./result.tsv")
             assert os.path.isdir(out)
 
-        # read Orthogroups.tsv
-        df = pd.read_csv(orthogroups_tsv, sep='\t')
-        df.set_index('Orthogroup', inplace=True)
+        # read "N0.tsv" and drop extra columns
+        df = pd.read_csv(orthogroups_tsv, sep='\t').drop(columns=['OG', 'Gene Tree Parent Clade'])
+        df.set_index('HOG', inplace=True)
 
         def get_gene_name(identifer: str):
             identifer = identifer.split(' ', maxsplit=1)[1].split(' [', maxsplit=1)[0]
@@ -26,14 +30,21 @@ class OrthogroupToGeneName():
             return identifer.split(' ', maxsplit=1)[0]
 
         def get_gene_id_to_name_dict(strain):
-            fasta_file_path = os.path.join(fasta_dir + F'/{strain}.faa')
+            fasta_file_path = os.path.join(fasta_dir + F'/{strain}.fasta')
             assert os.path.isfile(fasta_file_path), F'fasta file "{fasta_file_path}" is missing!'
             genes = SeqIO.parse(fasta_file_path, "fasta")
             return {gene.id: gene.description.split(' ', maxsplit=1)[1] for gene in genes}
 
         def gene_id_to_gene_name(gene_ids, gene_id_to_name):
             if isinstance(gene_ids, float): return []
-            return [gene_id_to_name[gene_id] for gene_id in gene_ids.split(', ')]
+            if gene_ids is None: return []
+
+            try:
+                return [gene_id_to_name[gene_id] for gene_id in gene_ids.split(', ')]
+
+            except KeyError:
+                print("Genes not present in fasta file: \n", gene_ids)
+                return []
 
         strains = df.columns
         for strain in strains:
@@ -41,7 +52,12 @@ class OrthogroupToGeneName():
             df[strain] = df[strain].apply(gene_id_to_gene_name, args=([gene_id_to_name]))
 
         def majority_vote(row, best_only=False):
-            all_names = [name for cell in row for name in cell]
+            """ Get gene names set per HOG and count them. Disregards names with eAED in description, 
+            typically unusefull maker annotations"""
+
+            all_names = [name for cell in row for name in cell if "eAED" not in name]
+            if not all_names:  all_names = ["NO DESCRIPTION"]  # if all names contain "eAED", all_names is an empty list
+
             if best_only:
                 return max(set(all_names), key=all_names.count)
 
